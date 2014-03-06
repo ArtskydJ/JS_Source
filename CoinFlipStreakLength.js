ff1d5bb6a2f62ba652ecdeee473bd1c321c1a023
Joseph Dykstra <josephdykstra@gmail.com>
2014-03-06 16:32:17 -0600

    deleted a few semicolons

--- CoinFlipStreakLength.js
+++ CoinFlipStreakLength.js
@@ -1,15 +1,15 @@
 var targetStreak = 28 //28 takes ~8 sec, 300 million flips
 
 function addCommas(nStr) { //http://www.mredkj.com/javascript/nfbasic.html
-	nStr += '';
-	x = nStr.split('.');
-	x1 = x[0];
-	x2 = x.length > 1 ? '.' + x[1] : '';
-	var rgx = /(\d+)(\d{3})/;
+	nStr += ''
+	x = nStr.split('.')
+	x1 = x[0]
+	x2 = x.length > 1 ? '.' + x[1] : ''
+	var rgx = /(\d+)(\d{3})/
 	while (rgx.test(x1)) {
-		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+		x1 = x1.replace(rgx, '$1' + ',' + '$2')
 	}
-	return x1 + x2;
+	return x1 + x2
 }
 
 function randomInt(num) {

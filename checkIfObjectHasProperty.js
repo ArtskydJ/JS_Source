316a6e445c4a0d9eafff0025306cd365dd007e49
Joseph Dykstra <josephdykstra@gmail.com>
2014-03-06 17:04:49 -0600

    Added SnakeJavascript8.js and checkIfObjectHasProperty.js

---
+++ checkIfObjectHasProperty.js
@@ -0,0 +1,10 @@
+﻿var obj = { joe:4, lee:8, dyk:2 }
+
+console.log(obj)
+Object.keys(obj).forEach(function (IN) {
+	console.log(obj[IN])
+})
+
+obj["joseph"] = 42 //The answer to life, the universe, and everything
+
+console.log(obj)

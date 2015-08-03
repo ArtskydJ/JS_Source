bd069be9f74802a0af4ff2d36b860785a278e931
Joseph Dykstra <josephdykstra@gmail.com>
2015-08-03 08:58:07 -0500

    style

--- hashMe.js
+++ hashMe.js
@@ -1,8 +1,10 @@
 var md5 = require('crypto').createHash('md5')
-
-require('fs').ReadStream('./hashMe.txt').on('data', function(d) {
-	md5.update(d)
-}).on('end', function() {
-	var hash = md5.digest('hex')
-    console.log("Hash:\n"+hash);
-})
+var fs = require('fs')
+fs.createReadStream('./hashMe.txt')
+	.on('data', function(d) {
+		md5.update(d)
+	})
+	.on('end', function() {
+		var hash = md5.digest('hex')
+		console.log("Hash:\n"+hash);
+	})
